1c553e068a98e4a91aa26cbb7937eb3e3aeddcdc
GHSVS-de <contact-VolliSchlothauer@ghsvs.de>
2023-10-04 00:48:03 +0200

    Update headerSectionBackground.php

--- src/html/mod_custom/headerSectionBackground.php
+++ src/html/mod_custom/headerSectionBackground.php
@@ -2,6 +2,7 @@
 /*
 GHSVS 2023-07-31.
 Das Hintergrundbild wird auf die gesamte #headerSection angewendet.
+Weiters enthält das Modul im Normalfall h1 und h2 Site-Titel (im Editor eingegeben).
  */
 
 defined('_JEXEC') or die;
@@ -14,11 +15,12 @@ $modId = 'mod-custom' . $module->id;
 
 if ($params->get('backgroundimage'))
 {
-    $wa = Factory::getApplication()->getDocument()->getWebAssetManager();
-    $wa->addInlineStyle('
-#headerSectionssss, .astroid-container{background-image: url("' . Uri::root(true) . '/' . HTMLHelper::_('cleanImageURL', $params->get('backgroundimage'))->url . '");}
-', ['name' => $modId]);
+	// Das Bild wird "globaler" eingesetzt und nicht nur hinter das eigentliche Modul!
+	Factory::getApplication()->getDocument()->getWebAssetManager()->addInlineStyle(
+		'.astroid-container{background-image: url("' . Uri::root(true) . '/' . HTMLHelper::_('cleanImageURL', $params->get('backgroundimage'))->url . '");}',
+		['name' => $modId]
+	);
 } ?>
-<div id="<?php echo $modId; ?>" class="mod-custom custom">
+<div id="<?php echo $modId; ?>" class="mod-custom custom siteTitle">
     <?php echo $module->content; ?>
 </div>
